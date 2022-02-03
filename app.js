8c8e99190285eea7e9860908aee9a2ab31644b3b
KaanBoz <bozkaan@hotmail.com>
2022-02-03 19:01:19 +0300

    testing 1

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World from getir-assignment-kb!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`getir-assingment-kb listenin on port: ${port}`)
+// })
+
+
 const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World from getir-assignment-kb!')
-})
-
-app.listen(port, () => {
-  console.log(`getir-assingment-kb listenin on port: ${port}`)
-})
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('/', (req, res) => res.send('Hello World from getir-assignment-kb!'))
+  .listen(PORT, () => console.log(`getir-assingment-kb listenin on port: ${ PORT }`))
